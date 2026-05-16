ce2220af34ab418bacd42eb1787a713681efbd0b
vivek <vivek28202028@gmail.com>
2026-05-16 12:01:09 +0530

    array 1st day commit

---
+++ Array/ArrayInput.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class ArrayInput{
+    public static void main(String[] args) {
+        int marks[]=new int[100];
+        Scanner sc=new Scanner(System.in);
+        marks[0]=sc.nextInt();
+        marks[1]=sc.nextInt();
+        marks[2]=sc.nextInt();
+         System.out.println("phys"+marks[0]);
+         System.out.println("chem"+marks[1]);
+         System.out.println("bio"+marks[2]);
+         
+    }
+}   
